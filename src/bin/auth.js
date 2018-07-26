fcdeae899ad84c6a697b90f79a7eb44bf63e07bf
Icaro Tavares <icarotav4@gmail.com>
2018-07-26 03:54:23 -0300

    Authentication fixed

--- src/bin/auth.js
+++ src/bin/auth.js
@@ -1,14 +1,15 @@
 const passport = require('passport')
 const { Strategy, ExtractJwt } = require('passport-jwt')
 const {
-  _,
+  __,
   eqProps,
   isNil,
   complement,
 } = require('ramda')
 const jwt = require('jsonwebtoken')
 const cfg = require('../config/config')
-const { userService } = require('../services/user')
+const { adminService } = require('../services/admin')
+const { alunoService } = require('../services/alunos')
 
 const params = {
   secretOrKey: cfg.security.jwtSecret,
@@ -18,17 +19,26 @@ const params = {
 const isNotNil = complement(isNil)
 
 module.exports = () => {
-  const strategy = new Strategy(params, (payload, done) => {
-    userService.findUser(payload.matricula)
-      .then((user) => {
-        if (!user) return done(null, false)
-        const userToken = jwt.decode(user.access_token)
-        const tokenAndPayloadEqProps = eqProps(_, userToken, payload)
-        if (isNotNil(userToken) && tokenAndPayloadEqProps('id') && tokenAndPayloadEqProps('iat') && tokenAndPayloadEqProps('exp')) {
-          return done(null, user)
-        }
-        return done(null, false)
-      }).catch(err => done(err, null))
+  const authUser = async (done, payload, service) => {
+    const user = await service.findOne(payload.matricula)
+    if (!user) return done(null, false)
+    const userToken = jwt.decode(user.access_token)
+    const tokenAndPayloadEqProps = eqProps(__, userToken, payload)
+    if (isNotNil(userToken) && tokenAndPayloadEqProps('isAdmin') && tokenAndPayloadEqProps('matricula') && tokenAndPayloadEqProps('iat') && tokenAndPayloadEqProps('exp')) {
+      return done(null, user)
+    }
+    return done(null, false)
+  }
+
+  const strategy = new Strategy(params, async (payload, done) => {
+    try {
+      return await authUser(done, payload, adminService)
+    } catch (err) {
+      if (err._response && err._response.statusCode === 404) { // eslint-disable-line no-underscore-dangle
+        return authUser(done, payload, alunoService)
+      }
+      return done(err, null)
+    }
   })
 
   passport.use(strategy)
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const { userController } = require('../controllers/user')
+const auth = require('../bin/auth')
 
 const router = express.Router()
 
 router.route('/login')
+  .get(auth().authenticate(), (req, res, next) => res.send('Auth test OK!')) // eslint-disable-line no-unused-vars
   .post((req, res, next) => userController.login(req, res, next))
 
 module.exports = router
